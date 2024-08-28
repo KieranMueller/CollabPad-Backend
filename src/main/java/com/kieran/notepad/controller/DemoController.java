d8a10d83bb05744464fd9ce8a3679d5467e22da1
KieranMueller <kieran98mueller@gmail.com>
2024-08-28 17:24:40 -0500

    add external variable

--- src/main/java/com/kieran/notepad/controller/DemoController.java
+++ src/main/java/com/kieran/notepad/controller/DemoController.java
@@ -1,13 +1,17 @@
 package com.kieran.notepad.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Value("${sample.greeting}")
+    private String greeting;
+
     @GetMapping("/greeting")
     public String greeting() {
-        return "Hello there!";
+        return greeting;
     }
 }
